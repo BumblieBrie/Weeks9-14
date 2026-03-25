29699d1124142a0d919fd8cc1bca4c7f51949e84
LAPTOP-6EK9OKR7\Owner <brie.noellee@gmail.com>
2026-03-25 10:14:47 -0400

    finished the random footsteps coding gym

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
-    public AudioSource footstepSFX;
+
+    // Done in AnimationEvent scene to play footstep sound effects at the right time during the walking animation
+    public int randomNumber;
+    public AudioSource footstep1SFX;
+    public AudioSource footstep2SFX;
+    public AudioSource footstep3SFX;
+    public AudioSource footstep4SFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,8 +23,25 @@ public class Knight : MonoBehaviour
 
     public void Footstep()
     {
+        randomNumber = Random.Range(1, 5); // Generate a random number between 1 and 4
         // Play footstep sound effect
-        footstepSFX.Play();
+
+        if (randomNumber == 1)
+        {
+            footstep1SFX.Play();
+        }
+        else if (randomNumber == 2)
+        {
+            footstep2SFX.Play();
+        }
+        else if (randomNumber == 3)
+        {
+            footstep3SFX.Play();
+        }
+        else if (randomNumber == 4)
+        {
+            footstep4SFX.Play();
+        }
 
         Debug.Log("Knight footstep sound played.");
     }
